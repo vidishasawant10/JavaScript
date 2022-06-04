5d963d0a9083e529f3514f08d31dc1b8af255245
sawantvidisha10 <sawantvidisha10@gmail.com>
2022-06-03 17:07:50 -0700

    updates

---
+++ index.js
@@ -0,0 +1,18 @@
+console.log('hello');
+let name = 'Mosh';
+let person={
+    name: 'Vidisha',
+    age: 30,
+};
+
+// Dot notation
+person.name = 'John';
+console.log(person.name);
+
+// Bracket notation
+person['name'] = 'Mary';
+console.log(person.name);
+
+
+
+
